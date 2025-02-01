6dd5e6083f5a71d7a584fe1d43333c508a23f93f
Asil Taufiqurrahman <asilt94@gmail.com>
2025-02-01 11:07:17 +0900

    update routes

--- routes/web.php
+++ routes/web.php
@@ -20,27 +20,27 @@ Route::get('/billing-pembayaran', [BillingController::class, 'billing_pembayaran
 Route::get('/tambah-billing',[BillingController::class, 'create_billing'] )->middleware(['auth', 'role:admin'])->name('billing.tambah');
 Route::post('/tambah-billing',[BillingController::class, 'store_billing'] )->middleware(['auth', 'role:admin'])->name('billing.store');
 
-Route::get('/billing-ukt', [BillingController::class, 'billing_ukt'])->middleware(['auth'])->name('billing.ukt');
-Route::get('/billing-ukt/edit/{id}', [BillingController::class, 'edit_billing_ukt'])->middleware(['auth'])->name('billing.ukt.edit');
-Route::patch('/billing-ukt/edit/{id}', [BillingController::class, 'update_billing_ukt'])->middleware(['auth'])->name('billing.ukt.update');
-Route::post('/billing-ukt/lunas', [BillingController::class, 'set_lunas_billing'])->middleware(['auth'])->name('billing.ukt.lunas');
-Route::get('/billing-ukt/import-data', [DataImportController::class, 'import_data_ukt_form'])->middleware(['auth'])->name('ukt.import.form');
-Route::post('/billing-ukt/import-data', [DataImportController::class, 'import_data_ukt'])->middleware(['auth'])->name('ukt.import');
+Route::get('/billing-ukt', [BillingController::class, 'billing_ukt'])->middleware(['auth', "role:admin|spp|keuangan"])->name('billing.ukt');
+Route::get('/billing-ukt/edit/{id}', [BillingController::class, 'edit_billing_ukt'])->middleware(['auth', "role:admin|spp|keuangan"])->name('billing.ukt.edit');
+Route::patch('/billing-ukt/edit/{id}', [BillingController::class, 'update_billing_ukt'])->middleware(['auth', "role:admin|spp|keuangan"])->name('billing.ukt.update');
+Route::post('/billing-ukt/lunas', [BillingController::class, 'set_lunas_billing'])->middleware(['auth', "role:admin|spp|keuangan"])->name('billing.ukt.lunas');
+Route::get('/billing-ukt/import-data', [DataImportController::class, 'import_data_ukt_form'])->middleware(['auth', "role:admin|spp|keuangan"])->name('ukt.import.form');
+Route::post('/billing-ukt/import-data', [DataImportController::class, 'import_data_ukt'])->middleware(['auth', "role:admin|spp|keuangan"])->name('ukt.import');
 
-Route::get('/billing-umb', [BillingController::class, 'billing_umb'])->middleware(['auth'])->name('billing.umb');
+Route::get('/billing-umb', [BillingController::class, 'billing_umb'])->middleware(['auth','role:admin|spp|keuangan'])->name('billing.umb');
 
 Route::get('/billing-dosen', [BillingController::class, 'billing_dosen'])->middleware(['auth'])->name('billing.dosen');
 Route::get('/billing-dosen/tambah', [BillingController::class, 'create_billing_dosen'])->middleware(['auth'])->name('billing.dosen.tambah');
 Route::post('/billing-dosen/tambah', [BillingController::class, 'store_billing_dosen'])->middleware(['auth'])->name('billing.dosen.store');
 
-Route::get('/jenis-bayar', [JenisBayarController::class, 'index'])->middleware(['auth'])->name('jenis-bayar');
-Route::get('/jenis-bayar/tambah', [JenisBayarController::class, 'create'])->middleware(['auth'])->name('jenis-bayar.tambah');
-Route::post('/jenis-bayar/tambah', [JenisBayarController::class, 'store'])->middleware(['auth'])->name('jenis-bayar.store');
+Route::get('/jenis-bayar', [JenisBayarController::class, 'index'])->middleware(['auth', 'role:developper|admin'])->name('jenis-bayar');
+Route::get('/jenis-bayar/tambah', [JenisBayarController::class, 'create'])->middleware(['auth', 'role:developper|admin'])->name('jenis-bayar.tambah');
+Route::post('/jenis-bayar/tambah', [JenisBayarController::class, 'store'])->middleware(['auth', 'role:developper|admin'])->name('jenis-bayar.store');
 
 Route::get('/tahun-pembayaran', [TahunPembayaranController::class, 'index'])
-    ->middleware(['auth'])->name('tahun-pembayaran');
+    ->middleware(['auth', 'role:developper|admin'])->name('tahun-pembayaran');
 Route::post('/tahun-pembayaran', [TahunPembayaranController::class, 'store'])
-    ->middleware(['auth'])->name('tahun-pembayaran.store');
+    ->middleware(['auth', 'role:developper|admin'])->name('tahun-pembayaran.store');
 
 
 Route::get('/tes', function () {
