7ecd43d1af48c2a84787f98c23ae181b8a419f7b
bangsile <asilt94@gmail.com>
2025-04-28 23:42:59 +0900

    rename nama method di route/api

--- routes/api.php
+++ routes/api.php
@@ -42,7 +42,7 @@ Route::get('/tahun-pembayaran', function (Request $request) {
 
 // E-Billing Mahasiswa
 Route::post('/billing-mahasiswa', [BillingMhsController::class, 'store_billing_mahasiswa'])->withoutMiddleware(['throttle:api']);
-Route::post('/billing-mahasiswa/detail', [BillingMhsController::class, 'get_detail_ukt'])->withoutMiddleware(['throttle:api']);
+Route::post('/billing-mahasiswa/detail', [BillingMhsController::class, 'get_detail_mahasiswa'])->withoutMiddleware(['throttle:api']);
 Route::patch('/billing-mahasiswa/update', [BillingMhsController::class, 'update_billing_mahasiswa'])->withoutMiddleware(['throttle:api']);
 
 // Manajemen E-Billing
